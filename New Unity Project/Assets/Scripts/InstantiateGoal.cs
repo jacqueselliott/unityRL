144d67f525aea58dfccdf1558e05edf4962ff785
Jacques Elliott <jge29@cam.ac.uk>
2017-01-16 20:39:30 +0000

    first commit

---
+++ New Unity Project/Assets/Scripts/InstantiateGoal.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantiateGoal : MonoBehaviour {
+
+    public GameObject goal;
+
+	// Use this for initialization
+	void Start () {
+        CreateGoal();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (gameObject.transform.childCount == 0)
+        {
+            CreateGoal();
+        }
+	}
+
+    void CreateGoal()
+    {
+        float randx = Random.Range(-4.5f, 4.5f);
+        float randz = Random.Range(-4.5f, 4.5f);
+        GameObject goalClone = (GameObject)Instantiate(goal, new Vector3(randx, 1f, randz), Quaternion.identity);
+        goalClone.transform.SetParent(gameObject.transform);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    private Rigidbody droneRigidbody;
+    public float forceMagnitude = 500000;
+
+	// Use this for initialization
+	void Start () {
+        droneRigidbody = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float inx = Input.GetAxis("Horizontal");
+        float inz = Input.GetAxis("Vertical");
+        float delta = Time.deltaTime;
+        Vector3 direction = new Vector3(inx, 0, inz)*delta*forceMagnitude;
+        droneRigidbody.AddForce(direction);
+        transform.eulerAngles = new Vector3(0f, transform.eulerAngles.y, 0f);
+	}
+}
