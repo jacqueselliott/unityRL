72379df7464607f38ef3c0e0d26c3d8bbdf1de79
Jacques Elliott <jge29@cam.ac.uk>
2017-01-17 18:47:24 +0000

    net controlled drone

--- unity_project/Assets/Scripts/DataTrack.cs
+++ unity_project/Assets/Scripts/DataTrack.cs
@@ -12,18 +12,38 @@ public class DataTrack : MonoBehaviour {
     private Vector3 droneVelocity;
     private Vector3 goalCoords;
 
-	// Use this for initialization
-	void Start () {
+    private Vector3 dir0;
+    private Vector3 dir1;
+    private Vector3 dir2;
+    private Vector3 dir3;
+
+    private Movement droneMovement;
+
+    // Use this for initialization
+    void Start () {
+        dir0 = new Vector3(1f, 0f, 0f);
+        dir1 = new Vector3(0f, 0f, 1f);
+        dir2 = new Vector3(0f, 0f, -1f);
+        dir3 = new Vector3(-1f, 0f, 0f);
         drone = GameObject.FindGameObjectWithTag("Drone");
+        droneMovement = drone.GetComponent<Movement>();
         droneRigidbody = drone.GetComponent<Rigidbody>();
         goal = null;
 		ws_cur = new WebSocket ("ws://localhost:9001");
 		ws_cur.OnMessage += (sender, e) => {
 			if (e.IsText) {
+                if (!droneMovement.netControlled)
+                {
+                    droneMovement.netControlled = true;
+                }
 				Debug.Log(e.Data.ToString());
 				//Handles received action
 				int action = int.Parse(e.Data.ToString());
-			}
+                if (action == 0) { droneMovement.direction = dir0; }
+                else if (action == 1) { droneMovement.direction = dir1; }
+                else if (action == 2) { droneMovement.direction = dir2; }
+                else if (action == 3) { droneMovement.direction = dir3; }
+            }
 		};
 		ws_cur.Connect ();
 		ws_cur.Send ("unity");
--- unity_project/Assets/Scripts/Movement.cs
+++ unity_project/Assets/Scripts/Movement.cs
@@ -5,6 +5,9 @@ public class Movement : MonoBehaviour {
 
     private Rigidbody droneRigidbody;
     public float forceMagnitude = 500000;
+    public Vector3 direction;
+    public bool netControlled = false;
+    private Vector3 newDirection;
 
 	// Use this for initialization
 	void Start () {
@@ -13,11 +16,15 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float inx = Input.GetAxis("Horizontal");
-        float inz = Input.GetAxis("Vertical");
+        if (!netControlled)
+        {
+            float inx = Input.GetAxis("Horizontal");
+            float inz = Input.GetAxis("Vertical");
+            direction = new Vector3(inx, 0, inz);
+        }
         float delta = Time.deltaTime;
-        Vector3 direction = new Vector3(inx, 0, inz)*delta*forceMagnitude;
-        droneRigidbody.AddForce(direction);
+        newDirection = direction*delta*forceMagnitude;
+        droneRigidbody.AddForce(newDirection);
         transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
         transform.eulerAngles = new Vector3(0f, transform.eulerAngles.y, 0f);
 	}
