a2ecff03d07dbcc07f1bd0ae402db8e3c7c37b73
Jacques Elliott <jge29@cam.ac.uk>
2017-01-17 03:25:16 +0000

    bigger is better

--- unity_project/Assets/Scripts/InstantiateGoal.cs
+++ unity_project/Assets/Scripts/InstantiateGoal.cs
@@ -20,8 +20,8 @@ public class InstantiateGoal : MonoBehaviour {
 
     void CreateGoal()
     {
-        float randx = Random.Range(-4.5f, 4.5f);
-        float randz = Random.Range(-4.5f, 4.5f);
+        float randx = Random.Range(-9.5f, 9.5f);
+        float randz = Random.Range(-9.5f, 9.5f);
         GameObject goalClone = (GameObject)Instantiate(goal, new Vector3(randx, 1f, randz), Quaternion.identity);
         goalClone.transform.SetParent(gameObject.transform);
     }
