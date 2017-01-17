14559db61e48a185f8333de3219331d62dcd54ff
Jacques Elliott <jge29@cam.ac.uk>
2017-01-17 03:34:29 +0000

    pointless push

--- unity_project/Assets/Scripts/InstantiateGoal.cs
+++ unity_project/Assets/Scripts/InstantiateGoal.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class InstantiateGoal : MonoBehaviour {
 
     public GameObject goal;
+    private float planeSize = 10f;
+    private GameObject drone;
 
 	// Use this for initialization
 	void Start () {
+        drone = GameObject.FindGameObjectWithTag("Drone");
         CreateGoal();
 	}
 	
@@ -20,9 +23,10 @@ public class InstantiateGoal : MonoBehaviour {
 
     void CreateGoal()
     {
-        float randx = Random.Range(-9.5f, 9.5f);
-        float randz = Random.Range(-9.5f, 9.5f);
-        GameObject goalClone = (GameObject)Instantiate(goal, new Vector3(randx, 1f, randz), Quaternion.identity);
+        float randx = Random.Range(planeSize-1f, planeSize-1f);
+        float randz = Random.Range(planeSize-1f, planeSize-1f);
+        Vector3 spawnLocation = new Vector3(randx, 1f, randz);
+        GameObject goalClone = (GameObject)Instantiate(goal, spawnLocation, Quaternion.identity);
         goalClone.transform.SetParent(gameObject.transform);
     }
 }
